bbbb551f4898ff0b4b23897b0ad02ffed3f1e7e0
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-05-07 15:53:41 +0300

    added fetch

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -10,14 +10,31 @@ export const fetchTrendMovies = async () => {
 
 export const fetchMoviesBySearch = async query => {
   const { data } = await axios.get(
-    `/search/movie?api_key=${API_KEY}&query=${query}`
+    `/search/movie?api_key=${API_KEY}&language=en-US&query=${query}`
   );
 
   return data;
 };
 
 export const fetchMovieById = async movieId => {
-  const { data } = await axios.get(`/movie/${movieId}?api_key=${API_KEY}`);
+  const { data } = await axios.get(
+    `/movie/${movieId}?api_key=${API_KEY}&language=en-US`
+  );
 
+  return data;
+};
+
+export const fetchMovieCast = async movieId => {
+  const { data } = await axios.get(
+    `/movie/${movieId}/credits?api_key=${API_KEY}&language=en-US`
+  );
+
+  return data;
+};
+
+export const fetchMovieReviews = async movieId => {
+  const { data } = await axios.get(
+    `/movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`
+  );
   return data;
 };
