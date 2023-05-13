a623eb6d63c11a48adc5cf6ce0df3dd45b4ddd1b
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-05-13 22:06:52 +0300

    fixed error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,32 @@
-import LoadingIndicator from 'components/Layout/LoadingIndicator';
-import MovieList from 'components/MovieList/MovieList';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchTrendMovies } from 'services/api';
+import LoadingIndicator from 'components/Layout/LoadingIndicator'; // Імпорт компонента LoadingIndicator
+import MovieList from 'components/MovieList/MovieList'; // Імпорт компонента MovieList
 
 const Home = () => {
-  const [trendingMovies, setTrendingMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [trendingMovies, setTrendingMovies] = useState([]); // Ініціалізація стану для списку трендових фільмів
+  const [isLoading, setIsLoading] = useState(true); // Ініціалізація стану для відображення індикатора завантаження
+  const [error, setError] = useState(false); // Ініціалізація стану для відображення повідомлення про помилку
 
   useEffect(() => {
     const fetchTrendingMovies = async () => {
       try {
         const { results } = await fetchTrendMovies();
-        setTrendingMovies(results); //записуємо result в trendingMovies
+        setTrendingMovies(results); // Запис результатів отриманих даних в стан trendingMovies
       } catch (error) {
-        setError(true);
+        setError(true); // Встановлення стану помилки при невдалому запиті
       } finally {
-        setIsLoading(false); // встановлення стану на завершення завантаження
+        setIsLoading(false); // Встановлення стану завершення завантаження
       }
     };
 
-    fetchTrendingMovies();
+    fetchTrendingMovies(); // Виклик функції для завантаження трендових фільмів
   }, []);
 
   return (
     <>
       {isLoading && <LoadingIndicator />}
+      {/* Відображення індикатора завантаження, якщо дані ще завантажуються */}
       {error ? (
         <p>
           Sorry, we could not fetch the trending movies. Please try again later.
@@ -33,6 +34,7 @@ const Home = () => {
       ) : (
         <MovieList trendingMovies={trendingMovies} />
       )}
+      {/* Відображення списку трендових фільмів */}
     </>
   );
 };
--- src/pages/MoviesDetails.jsx
+++ src/pages/MoviesDetails.jsx
@@ -1,34 +1,39 @@
-import MovieCard from 'components/MovieCard/MovieCard';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 import { fetchMovieById } from '../services/api';
 import { BsArrowLeftShort } from 'react-icons/bs';
 import { Button, Container } from './MoviesDetails.styled';
 import LoadingIndicator from 'components/Layout/LoadingIndicator';
 import { Suspense } from 'react';
+import MovieCard from 'components/MovieCard/MovieCard';
 
 const MoviesItem = () => {
   const { movieId } = useParams();
   const location = useLocation();
-  const [selectedMovie, setSelectedMovie] = useState({});
+  const [selectedMovie, setSelectedMovie] = useState(null); // Ініціалізація значення selectedMovie як null
 
   useEffect(() => {
     const fetchSelectedMovie = async movieId => {
       try {
         const movieData = await fetchMovieById(movieId);
-        setSelectedMovie(movieData);
+        setSelectedMovie(movieData); // Оновлення значення selectedMovie після завантаження даних про фільм
       } catch (error) {
         console.log(error);
       }
     };
 
-    fetchSelectedMovie(movieId);
+    fetchSelectedMovie(movieId); // Виклик функції fetchSelectedMovie для завантаження даних про фільм
   }, [movieId]);
+
+  if (!selectedMovie) {
+    // Рендерінг індикатора завантаження, поки дані про фільм не завантажено
+    return <LoadingIndicator />;
+  }
 
   return (
     <main>
       <Container>
-        {/* Повертає назад*/}
+        {/* Кнопка "Назад" */}
         <Link
           to={location?.state?.from ?? '/'}
           style={{ textDecoration: 'none' }}
@@ -40,9 +45,10 @@ const MoviesItem = () => {
             Go back
           </Button>
         </Link>
+        {/* Компонент MovieCard для відображення вибраного фільму */}
         <MovieCard movie={selectedMovie} />
         <Suspense fallback={<LoadingIndicator />}>
-          <Outlet />
+          <Outlet /> {/* Відображення вкладених маршрутів */}
         </Suspense>
       </Container>
     </main>
