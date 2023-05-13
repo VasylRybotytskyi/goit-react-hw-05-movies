dfa0719c70cd9b90ea1950c377237d4836a3500f
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-05-13 20:15:51 +0300

    added modern-normalize

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router';
 import { Home } from '../pages/Home';
 import { Movies } from '../pages/Movies';
 import MoviesItem from '../pages/MoviesDetails';
-import { Layout } from './Layout/Layout';
+import Layout from './Layout/Layout';
 import Cast from './Cast/Cast';
 import Reviews from './Reviews/Reviews';
+import { GlobalStyle } from './GlobalStyled';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="movies" element={<Movies />} />
-        <Route path="movies/:movieId" element={<MoviesItem />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MoviesItem />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<>NotFound</>} />
         </Route>
-        <Route path="*" element={<>NotFound</>} />
-      </Route>
-    </Routes>
+      </Routes>
+    </>
   );
 };
