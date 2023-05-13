dd66369609785fae67b0e77c87c3605ec78ab075
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-05-13 22:24:33 +0300

    update searchmovies

--- src/components/SearchMovies/SearchMovies.jsx
+++ src/components/SearchMovies/SearchMovies.jsx
@@ -1,35 +1,38 @@
-import Notiflix from 'notiflix';
+import Notiflix from 'notiflix'; // Імпорт бібліотеки Notiflix, яка використовується для виведення повідомлень
+
 import { Button, Form, Input } from './SearchMovies.styled';
+
 import PropTypes from 'prop-types';
 
 const SearchMovies = ({ onSubmit }) => {
   const handleSubmit = async e => {
-    e.preventDefault();
+    e.preventDefault(); // Заборона стандартної поведінки форми
 
-    const query = e.target.elements.query.value;
+    const query = e.target.elements.query.value; // Отримання значення з поле пошукового запиту
 
     if (!query.trim()) {
-      Notiflix.Notify.failure('Please enter something');
+      Notiflix.Notify.failure('Please enter something'); // Виведення повідомлення про помилку, якщо пошуковий запит порожній
       return;
     }
 
     try {
-      const results = await onSubmit(query);
+      const results = await onSubmit(query); // Виклик функції onSubmit з переданим пошуковим запитом
 
       if (results.length === 0) {
-        Notiflix.Notify.failure('No movies found'); // Display error for no search results
+        Notiflix.Notify.failure('No movies found'); // Виведення повідомлення про помилку, якщо немає результатів пошуку
       }
     } catch (error) {
-      Notiflix.Notify.failure('An error occurred. Please try again later.'); // Display error for API failure
+      Notiflix.Notify.failure('An error occurred. Please try again later.'); // Виведення повідомлення про помилку при невдачі в API
     }
 
-    e.target.reset();
+    e.target.reset(); // Очищення значень полів форми
   };
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Input name="query" type="text" placeholder="Search movies" />
-      <Button>Search</Button>
+      <Input name="query" type="text" placeholder="Search movies" />{' '}
+      {/* Поле вводу пошукового запиту */}
+      <Button>Search</Button> {/* Кнопка пошуку */}
     </Form>
   );
 };
