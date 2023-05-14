ee46f139dde5e8e32cdf78087fa92e7156ab9e8d
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-05-14 10:01:08 +0300

    added lazu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router';
-import Home from '../pages/Home';
-import Movies from '../pages/Movies';
-import MoviesItem from '../pages/MoviesDetails';
-import Layout from './Layout/Layout';
-import Cast from './Cast/Cast';
-import Reviews from './Reviews/Reviews';
+import { lazy } from 'react';
+
 import { GlobalStyle } from './GlobalStyled';
+
+const Layout = lazy(() => import('./Layout/Layout'));
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MoviesDetails = lazy(() => import('../pages/MoviesDetails'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 export const App = () => {
   return (
@@ -15,7 +18,7 @@ export const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="movies" element={<Movies />} />
-          <Route path="movies/:movieId" element={<MoviesItem />}>
+          <Route path="movies/:movieId" element={<MoviesDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
