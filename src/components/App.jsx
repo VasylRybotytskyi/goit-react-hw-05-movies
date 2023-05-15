a77360cbe9e562774e4c3f9f9bdde59ce976f803
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-05-16 00:37:14 +0300

    fix error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { lazy } from 'react';
 
 import { GlobalStyle } from './GlobalStyled';
@@ -22,7 +22,7 @@ export const App = () => {
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
           </Route>
-          <Route path="*" element={<>NotFound</>} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
     </>
